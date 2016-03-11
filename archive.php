f892d35dbd0c26bb660d743b95875eacf0c9227b
Jason Halsey <jasonhalsey@Halsey-C2QNF00HFTPR.local>
2016-03-11 08:58:22 -0800

    make portfolio show in categories widget results

--- archive.php
+++ archive.php
@@ -1,42 +1,47 @@
 <?php
 /*
-Template Name: Portfolio Archive
+/**
+ * The template for displaying archive pages
+ *
+ * Used to display archive-type pages if nothing more specific matches a query.
+ * For example, puts together date-based pages if no date.php file exists.
+ *
+ * If you'd like to further customize these archive views, you may create a
+ * new template file for each one. For example, tag.php (Tag archives),
+ * category.php (Category archives), author.php (Author archives), etc.
 */
 
  get_header('interior'); ?>
 <section class="dark-stripe">
     <h2>
-      Recent Projects 2
+      Archive Template
     </h2>
 
     <div id="project-container">
-        <?php  
-            $args = array(
+  
+      <?php if (!function_exists('dynamic_sidebar') || !dynamic_sidebar('Top Sidebar')) : ?>
+ 
+      <?php endif; ?>
+
+        <?php 
+          $args_r = array(
                 'post_type'      => 'portfolio',
-                'meta_key'       => '_cmb2_portfolio_checkbox',
-                'meta_value'     => 'on',
                 'order'          => 'ASC',
                 'posts_per_page' => 10,
             );
         ?>
-  <?php if (!function_exists('dynamic_sidebar') || !dynamic_sidebar('Top Sidebar')) : ?>
- 
-      <?php endif; ?> 
-
-  
-        <?php $the_query = new WP_Query( $args ); ?>
-        <?php if ( $the_query->have_posts() ) : ?>
-
-
-            <?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+          <?php $the_query_r = new WP_Query( $args_r ); ?>
+          <?php if ( $the_query_r->have_posts() ) : ?>
+            <?php
+              // Start the Loop.
+            while ( have_posts() ) : the_post(); ?>
 
             <?php $src = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), array( 5600,1000 ), false, '' ); ?>
-
             <div class="single-project">
                 <div class="hover-tile-outer"  style="background-image: url(<?php echo $src[0]; ?> ) !important; background-size: cover!important; background-position: 0 50%;">
                   <div class="hover-tile-container">
                     <div class="hover-tile hover-tile-visible">
-                      <span><?php the_title(); ?></span>
+                      <span><?php the_title(); ?> </span>
                     </div>
                     <div class="hover-tile hover-tile-hidden">
                       <h4><?php the_title(); ?></h4>
