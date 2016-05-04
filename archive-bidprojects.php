c53d00e9683ef8a1373a56ed73304f8bfb40dbb9
Jason Halsey <jasonhalsey@Halsey-C2QNF00HFTPR.local>
2016-05-04 10:11:08 -0700

    Add fallback to bid page

--- archive-bidprojects.php
+++ archive-bidprojects.php
@@ -13,29 +13,34 @@ Template Name: Bid Projects
                 $bidgroup = array( 'post_type' => 'bidproject', 'order' => 'Name');
                 $bidgrouploop = new WP_Query( $bidgroup );
             ?>
-            <?php while ( $bidgrouploop->have_posts() ) : $bidgrouploop->the_post();?>
-                <article id="post-<?php the_ID(); ?>" <?php post_class('project_container'); ?>>
-                    <div class="date-block">
-                        <h3>Due</h3>
-                        <h3><?php
-                            // Get unix date
-                            $some_date = get_post_meta( $post->ID, '_cmb2_project_datetime_timestamp', true );
-                            // Convert unix date
-                            $some_date = date( 'm/d', $some_date );
-                            // Print converted date
-                            echo $some_date;
-                        ?></h3>
-                    </div>
-                    <div class="project_information">
-            	    	<h2><a href="<?php the_permalink() ?>" ><?php the_title(); ?> <span class="bid_arrow">&#10140;</span></a></h2>
-                        <h4>
-                            <?php echo get_post_meta( $post->ID, '_cmb2_project_address', true ); ?>
-                            <?php echo get_post_meta( $post->ID, '_cmb2_project_city', true ); ?>, <?php echo get_post_meta( $post->ID, '_cmb2_project_state', true ); ?> <?php echo get_post_meta( $post->ID, '_cmb2_project_zip', true );
-                            ?>
-                        </h4>
-                    </div><!-- end  project_information -->
-          		</article>
-            <?php endwhile; ?>
+            <?php if ($bidgrouploop->have_posts()) { ?>
+                 <?php while ( $bidgrouploop->have_posts() ) : $bidgrouploop->the_post();?>
+                    <article id="post-<?php the_ID(); ?>" <?php post_class('project_container'); ?>>
+                        <div class="date-block">
+                            <h3>Due</h3>
+                            <h3><?php
+                                // Get unix date
+                                $some_date = get_post_meta( $post->ID, '_cmb2_project_datetime_timestamp', true );
+                                // Convert unix date
+                                $some_date = date( 'm/d', $some_date );
+                                // Print converted date
+                                echo $some_date;
+                            ?></h3>
+                        </div>
+                        <div class="project_information">
+                            <h2><a href="<?php the_permalink() ?>" ><?php the_title(); ?> <span class="bid_arrow">&#10140;</span></a></h2>
+                            <h4>
+                                <?php echo get_post_meta( $post->ID, '_cmb2_project_address', true ); ?>
+                                <?php echo get_post_meta( $post->ID, '_cmb2_project_city', true ); ?>, <?php echo get_post_meta( $post->ID, '_cmb2_project_state', true ); ?> <?php echo get_post_meta( $post->ID, '_cmb2_project_zip', true );
+                                ?>
+                            </h4>
+                        </div><!-- end  project_information -->
+                    </article>
+                <?php endwhile; ?>
+             <?php } else {?>
+                <h2>There are currently no projects up for bid.</h2>
+            <?php } ?>
+           
         </div><!-- end #content -->
     </div><!-- end #primary -->
 </section>
